bcd129cb8a0c85f2c18860b53527bf6d30ad213c
Kim-ChanMin <halfmoondeer@naver.com>
2020-11-06 00:45:29 +0900

    Add extracting jobs from indeed.com.

--- indeed.py
+++ indeed.py
@@ -2,14 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 LIMIT = 50
-URL = "https://kr.indeed.com/jobs?q=python&limit={LIMIT}&radius=25"
+URL = "https://www.indeed.com/jobs?q=python%20developer&limit={}&radius=25".format(LIMIT)
 
 def extract_indeed_pages() :
-    # indeed.com에서 python과 관련된 회사 페이지
-    indeed_result = requests.get(URL)
-    indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
-    # 주소에서 page와 관련된 부분 찾기
-    pagination = indeed_soup.find("div", {"class": "pagination"})
+
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pagination = soup.find("div", {"class": "pagination"})
 
     links = pagination.find_all('a')
     pages = []
@@ -17,10 +16,16 @@ def extract_indeed_pages() :
         pages.append(int(link.string))
 
     max_page = pages[-1]  # get last page
-    print(max_page)
     return max_page
 
 def extract_indeed_jobs(last_page) :
-    for page in range(last_page):
-        result = requests.get(f"{URL}&start = {page*LIMIT}")
-        print(result.status_code)
+
+    jobs = []
+    result = requests.get(f"{URL}&start = {0*LIMIT}") #in first page
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class" : "jobsearch-SerpJobCard"}) # html lists
+
+    for result in results:
+        title = result.find("h2", {"class": "title"}).find("a")["title"]
+
+    return jobs
