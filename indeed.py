67fad7b7183587c52b790f26d854e136cc200325
Kim-ChanMin <halfmoondeer@naver.com>
2020-11-06 01:02:57 +0900

    Add extracting companies from indeed.com.

--- indeed.py
+++ indeed.py
@@ -27,5 +27,13 @@ def extract_indeed_jobs(last_page) :
 
     for result in results:
         title = result.find("h2", {"class": "title"}).find("a")["title"]
-
+        company = result.find("span",{"class":"company"})
+        company_anchor = company.find("a") #Some company doesn't have a link
+        #too many blanks get out. -> use strip to solve
+        if company_anchor is not None :
+            company = str(company_anchor.string)
+        else :
+            company = str(company.string)
+        company = company.strip() #delete blank
+        print(title, company)
     return jobs
